f5bb95b6003a652c118cef04723d745ddf3a7900
Value Lee <value.lee@outlook.com>
2025-08-11 11:40:03 +0800

    。

---
+++ ValueLee.Nuart/SerialPortManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueLee.Nuart
+{
+    public class SerialPortManager : IDisposable
+    {
+        public SerialPortManager()
+        {
+        }
+
+        public void Create()
+        {
+        }
+
+        public void Release()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}

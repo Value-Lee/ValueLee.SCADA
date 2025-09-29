7065fb9f5e2200e3921304159075c2f8c72cbd31
Value Lee <value.lee@outlook.com>
2025-09-29 23:10:58 +0800

    。

---
+++ SCADA.PLCFramework/PlcAccessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA.PLCFramework
+{
+    public  class PlcAccessor:IPlcAccessor
+    {
+        private static PlcAccessor Instance { get; } = new PlcAccessor();
+        private PlcAccessor() { }
+
+        public  void Load(string settingFile)
+        {
+
+        }
+
+
+        public  void Add()
+        {
+
+        }
+
+        public  byte ReadByte(RWMode mode = RWMode.FromCache)
+        {
+
+        }
+    }
+}

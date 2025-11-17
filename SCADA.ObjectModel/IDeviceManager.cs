10b0502a5ed3c326726550fa25f107ac4b262216
Value Lee <value.lee@outlook.com>
2025-11-18 04:09:38 +0800

    。

---
+++ SCADA.ObjectModel/IDeviceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA.ObjectModel
+{
+    internal interface IDeviceManager
+    {
+        void AddDevice(IDevice device);
+        void RemoveDevice(IDevice device);
+        IDevice GetDeviceById(string deviceId);
+    }
+}
